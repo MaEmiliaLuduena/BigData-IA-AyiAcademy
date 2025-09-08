eaa6a9dd2b4477ee413b21f0d4c093ad4607c872
Maria <emiludu@gmail.com>
2025-09-08 14:35:33 -0300

    Descarga dataset desde Kaggle

--- bronze/split_dataset.py
+++ bronze/split_dataset.py
@@ -1,16 +1,27 @@
 import pandas as pd
 import os # Gestiona automáticamente las diferencias entre sistemas operativos como '/' en Linux/macOS o '\' en Windows
 
-# ----------------------------------------------------------------- #
-#    DIVIDIR EL DATASET ORIGINAL A Customers, Products y Orders     #
-# ----------------------------------------------------------------- #
+# -------------------------------- #
+#    DESCARGAR DATASET ORIGINAL    #
+# -------------------------------- #
+# Carpeta destino
+data_dir = "data/raw/"
+os.makedirs(data_dir, exist_ok=True)
 
-# Ruta al dataset original
-RAW_PATH = os.path.join("data", "raw", "Sample_Superstore.csv")
+# Descargar dataset de Kaggle
+os.system(f'kaggle datasets download -d vivek468/superstore-dataset-final -p {data_dir} --unzip')
 
-# Cargar dataset original
-df = pd.read_csv(RAW_PATH, encoding="ISO-8859-1")
+# Leer archivo descargado
+file_path = os.path.join(data_dir, "Sample - Superstore.csv")
+df = pd.read_csv(file_path, encoding="ISO-8859-1") # Utilizo encoding para poder leer el dataset. El original no está en UTF-8
 
+print("Primeras filas del dataset:")
+print(df.head())
+
+
+# ------------------------------------------------------------- #
+#    DIVIDIR DATASET ORIGINAL EN CUSTOMERS, PRODUCTS Y ORDERS   #
+# ------------------------------------------------------------- #
 # Customers
 customers = df[[
     "Customer ID", "Customer Name", "Segment", "Country",
